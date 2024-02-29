dcf7fb858b319aab154164e26a697eb0f4d12c37
Vyacheslav Boev <slava.boev94@gmail.com>
2024-02-29 16:27:19 +0300

    Add or-blending with benchmark

--- ydb/library/yql/utils/simd/exec/pack_tuple/main.cpp
+++ ydb/library/yql/utils/simd/exec/pack_tuple/main.cpp
@@ -9,6 +9,7 @@ struct TPerfomancer {
 
     struct TWrapWorker {
         virtual int PackTuple(bool log) = 0;
+        virtual void CmpPackTupleOrAndNaive() = 0;
         virtual ~TWrapWorker() = default;
     };
 
@@ -153,6 +154,261 @@ struct TPerfomancer {
             return PackTupleImpl(log);
         }
 
+        template <ui8 Cols>
+        static void
+        PackTupleNaiveImpl(const ui8 *const (&src_cols)[Cols],
+                          ui8 *const dst_rows, size_t size,
+                          const ui8 (&col_sizes)[Cols]) {
+            size_t tuple_total_size = 0;
+            size_t offsets[Cols];
+            for (ui8 col = 0; col != Cols; ++col) {
+                offsets[col] = tuple_total_size;
+                tuple_total_size += col_sizes[col];
+            }
+
+            for (size_t row = 0; row != size; ++row) {
+                for (ui8 col = 0; col != Cols; ++col) {
+                    memcpy(dst_rows + row * tuple_total_size + offsets[col],
+                           src_cols[col] + row * col_sizes[col],
+                           col_sizes[col]);
+                }
+            }
+        }
+
+        static TSimd<ui8>
+        BuildTuplePerm(ui8 col_size, ui8 col_pad, ui8 offset, ui8 ind) {
+            ui8 perm[TSimd<ui8>::SIZE];
+            std::memset(perm, 0x80, TSimd<ui8>::SIZE);
+
+            const ui8 size = col_size + col_pad;
+            ui8 iters = TSimd<ui8>::SIZE / size;
+
+            while (iters--) {
+                for (ui8 it = col_size; it; --it, ++offset, ++ind) {
+                    perm[offset] = ind;
+                }
+                offset += col_pad;
+            }
+
+            return TSimd<ui8>{perm};
+        }
+
+        template <ui8 TupleSize>
+        static TSimd<ui8> TupleOr(TSimd<ui8> (&vec)[TupleSize]) {
+            return TupleOrImpl<TupleSize>(vec);
+        }
+
+        template <ui8 TupleSize>
+        static TSimd<ui8> TupleOrImpl(TSimd<ui8> vec[]) {
+            static constexpr ui8 Left = TupleSize / 2;
+            static constexpr ui8 Right = TupleSize - Left;
+
+            return TupleOrImpl<Left>(vec) | TupleOrImpl<Right>(vec + Left);
+        }
+
+        template <> TSimd<ui8> TupleOrImpl<1>(TSimd<ui8> vec[]) {
+            return vec[0];
+        }
+
+        template <> TSimd<ui8> TupleOrImpl<2>(TSimd<ui8> vec[]) {
+            return vec[0] | vec[1];
+        }
+
+        template <ui8 StoresPerLoad, ui8 Cols>
+        static void
+        PackTupleOrImpl(const ui8 *const (&src_cols)[Cols],
+                           ui8 *const dst_rows, size_t size, const ui8 (&col_sizes)[Cols]) {
+            // ui8 type used for sizes as a reminder, 
+            // that tuple is small and shoud fit into one reg
+            ui8 tuple_total_size = 0;
+            for (ui8 col = 0; col != Cols; ++col) {
+                tuple_total_size += col_sizes[col];
+            };
+
+            const ui8 tuples_per_store = TSimd<ui8>::SIZE / tuple_total_size;
+            ui8 col_store_sizes[Cols];
+            for (ui8 col = 0; col != Cols; ++col) {
+                col_store_sizes[col] = col_sizes[col] * tuples_per_store;
+            };
+
+            TSimd<ui8> perms[Cols][StoresPerLoad];
+            for (ui8 col = 0, offset = 0; col != Cols; ++col) {    
+                for (ui8 ind = 0; ind != StoresPerLoad; ++ind) {
+                    perms[col][ind] = BuildTuplePerm( col_sizes[col],
+                                     ui8(tuple_total_size - col_sizes[col]),
+                                    offset,  ind * col_store_sizes[col]);
+                }
+                offset += col_sizes[col];
+            }
+
+            TSimd<ui8> src_regs[Cols];
+            TSimd<ui8> perm_regs[Cols];
+
+            const ui8 *srcs[Cols];
+            std::memcpy(srcs, src_cols, sizeof(srcs));
+
+            const size_t simd_iters = size / (tuples_per_store * StoresPerLoad);
+            ui8 *const end = dst_rows + simd_iters * tuples_per_store * StoresPerLoad * tuple_total_size;
+            
+            TSimd<ui8> tmp;
+
+            auto dst = dst_rows;
+            while (dst != end) {
+                for (ui8 col = 0; col != Cols; ++col) {
+                    src_regs[col] = TSimd<ui8>(srcs[col]);
+                    srcs[col] += col_store_sizes[col] * StoresPerLoad;
+                }
+
+                for (ui8 iter = 0; iter != StoresPerLoad; ++iter) {
+                    // shuffling each col bytes to the right positions
+                    // then blending them together with 'or'
+                    for (ui8 col = 0; col != Cols; ++col) {
+                      perm_regs[col] = src_regs[col].Shuffle(perms[col][iter]);
+                    }
+
+                    // tmp |= TupleOr(perm_regs);
+                    TupleOr(perm_regs).Store(dst);
+                    dst += tuple_total_size * tuples_per_store;
+                }
+            }
+
+            PackTupleNaiveImpl(srcs, dst,
+                              size - simd_iters * tuples_per_store * StoresPerLoad, col_sizes);
+        }
+
+        template <class... Types> struct PackBenchCase {
+            static constexpr size_t Iters = 1; // value > 1 affects inlining
+            static constexpr size_t Memory = 1 << 28;
+            static constexpr size_t TupleSize = (sizeof(Types) + ...);
+            static constexpr size_t Cols = sizeof...(Types);
+            static constexpr size_t NTuples = Memory / TupleSize;
+
+            static constexpr size_t StoresPerLoad =
+                std::min(Cols, TSimd<ui8>::SIZE / std::max({sizeof(Types)...}) /
+                                   (TSimd<ui8>::SIZE / TupleSize));
+            using PackTupleF = void (*)(const ui8 *const (&)[Cols], ui8 *const,
+                                        size_t, const ui8 (&)[Cols]);
+
+            static constexpr std::pair<PackTupleF, const char *> Runs[] = {
+                // order affects naive (memcpy) performance
+                {&PackTupleNaiveImpl<Cols>, "naive"},
+                {&PackTupleOrImpl<StoresPerLoad, Cols>, "or"},
+            };
+
+            static void Run() {
+#define INDEXED_TYPES(...)                                                     \
+    [&]<class... ATypes, size_t... Is>(const std::tuple<ATypes...> &,          \
+                                       std::index_sequence<Is...>) {           \
+      __VA_ARGS__                                                              \
+    }(std::tuple<Types...>{}, std::make_index_sequence<Cols>{})
+
+#define INDEXED_TYPES_B                                                        \
+    [&]<class... ATypes, size_t... Is>(const std::tuple<ATypes...> &,          \
+                                       std::index_sequence<Is...>) {
+
+#define INDEXED_TYPES_E                                                        \
+    }                                                                          \
+    (std::tuple<Types...>{}, std::make_index_sequence<Cols>{})
+
+                Cerr << "\n - cols byte alignment";
+                (..., (Cerr << ' ' << sizeof(Types)));
+                Cerr << '\n';
+
+                const ui8 col_sizes[Cols] = {sizeof(Types) ...};
+                size_t offsets[Cols];
+                for (ui8 col = 0, offset = 0; col != Cols; ++col) {
+                    offsets[col] = offset;
+                    offset += col_sizes[col];
+                }
+
+                std::unique_ptr<ui8[]> srcs[sizeof...(Types)];
+                const ui8* src_cols[Cols];
+
+                INDEXED_TYPES((srcs[Is].reset(new (std::align_val_t(
+                                   32)) ui8[NTuples * sizeof(ATypes)]),
+                               ...);
+                               ((src_cols[Is] = srcs[Is].get()), ...);
+                               );
+
+                for (size_t ind = 0; ind < NTuples; ind++) {
+                    INDEXED_TYPES(((reinterpret_cast<Types *>(
+                                        srcs[Is].get())[ind] = Cols * ind + Is),
+                                   ...););
+                }
+
+                std::unique_ptr<ui8[]> dst{new (std::align_val_t(32))
+                                               ui8[NTuples * TupleSize]};
+                ui8 *dst_rows = dst.get();
+
+                for (const auto &[func, name] : Runs) {
+                    Cerr << "\nrunning: " << name << '\n';
+                    for (ui8 iters = 0; iters != Iters; ++iters) {
+
+                      dst[0] = 1;
+                      for (size_t ind = 1; ind != NTuples * TupleSize; ++ind) {
+                        dst[ind] = dst[ind - 1] * 127 + 1;
+                      }
+
+                      const std::chrono::steady_clock::time_point begin_ts =
+                          std::chrono::steady_clock::now();
+
+                      std::invoke(func, src_cols, dst.get(), NTuples,
+                                  col_sizes);
+
+                      const std::chrono::steady_clock::time_point end_ts =
+                          std::chrono::steady_clock::now();
+                      const ui64 us =
+                          std::chrono::duration_cast<std::chrono::microseconds>(
+                              end_ts - begin_ts)
+                              .count();
+                      const size_t mbs =
+                          (NTuples * TupleSize * 1000ul * 1000ul) /
+                          (us * 1024ul * 1024ul + 1);
+
+                      size_t fails = 0;
+                      for (size_t row = 0; row < NTuples; row += 1) {
+                        bool flag = true;
+                        INDEXED_TYPES(flag = (flag && ... &&
+                                              (reinterpret_cast<Types *>(
+                                                   dst_rows + row * TupleSize +
+                                                   offsets[Is])[0] ==
+                                               Types(Cols * row + Is))););
+                        if (flag) {
+                          continue;
+                        }
+                        ++fails;
+
+                        INDEXED_TYPES(
+                            (...,
+                             (Cerr << row << ": "
+                                   << size_t(reinterpret_cast<Types *>(
+                                          dst_rows + row * TupleSize +
+                                          offsets[Is])[0])
+                                   << " =?= " << (Cols * row + Is) << '\n'));
+
+                        );
+                      }
+
+                      Cerr << mbs << " mib/s, fails: " << fails << '\n';
+                    }
+                }
+
+#undef INDEXED_TYPES
+            }
+        };
+
+        void CmpPackTupleOrAndNaive() override {
+            Cerr << " --- Compare simd-or and naive copy --- \n";
+
+            PackBenchCase<ui64>::Run();
+            PackBenchCase<ui32, ui32>::Run();
+            PackBenchCase<ui32, ui32, ui32>::Run();
+            PackBenchCase<ui32, ui32, ui32, ui32>::Run();
+            PackBenchCase<ui8, ui16, ui32>::Run();
+            PackBenchCase<ui8, ui16, ui8, ui8>::Run();
+            PackBenchCase<ui16, ui16, ui16, ui16>::Run();
+        }
+
         ~TWorker() = default;
     };
 
@@ -171,5 +427,8 @@ int main() {
 
     bool fine = true;
     fine &= worker->PackTuple(false);
+
+    worker->CmpPackTupleOrAndNaive();
+
     return !fine;
 }
