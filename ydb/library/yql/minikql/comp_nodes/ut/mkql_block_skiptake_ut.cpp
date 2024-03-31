9a69c1c8b642f6fd3bc18a1cd6d393aab8c92111
san-kir-k <dfomka@list.ru>
2024-03-31 22:52:44 +0300

    Add possible implementation of new node
    But it still doesnt compile for some reason...

--- ydb/library/yql/minikql/comp_nodes/ut/mkql_block_skiptake_ut.cpp
+++ ydb/library/yql/minikql/comp_nodes/ut/mkql_block_skiptake_ut.cpp
@@ -1,11 +1,28 @@
 #include "mkql_computation_node_ut.h"
 
-#include <ydb/library/yql/minikql/arrow/arrow_defs.h>
+#include <ydb/library/yql/minikql/computation/mkql_block_reader.h>
+#include <ydb/library/yql/minikql/computation/mkql_block_builder.h>
+#include <ydb/library/yql/minikql/computation/mkql_block_impl.h>
 #include <ydb/library/yql/minikql/computation/mkql_computation_node_holders.h>
-#include <ydb/library/yql/minikql/computation/mkql_computation_node_codegen.h> // Y_IGNORE
 
+#include <ydb/library/yql/minikql/arrow/arrow_defs.h>
+#include <ydb/library/yql/minikql/arrow/arrow_util.h>
+#include <ydb/library/yql/minikql/mkql_type_builder.h>
+#include <ydb/library/yql/minikql/computation/mkql_computation_node_codegen.h>  // Y_IGNORE
+#include <ydb/library/yql/minikql/mkql_node_builder.h>
+#include <ydb/library/yql/minikql/mkql_node_cast.h>
+
+#include <ydb/library/yql/parser/pg_wrapper/interface/arrow.h>
+
+#include <ydb/library/yql/utils/simd/simd.h>
+
+#include <arrow/scalar.h>
+#include <arrow/array.h>
+#include <arrow/datum.h>
 #include <arrow/array/builder_primitive.h>
+
 #include <chrono>
+#include <numeric>
 
 namespace NKikimr {
 namespace NMiniKQL {
@@ -129,80 +146,22 @@ public:
 
     EFetchResult DoCalculate(NUdf::TUnboxedValue& state, TComputationContext& ctx, NUdf::TUnboxedValue*const* output) const
     {
-        auto& s = GetState(state);
+        auto& s = GetState(state, ctx);
         const auto fields = ctx.WideFields.data() + WideFieldsIndex_;
-        if (s.Current_ == s.BlockLength_) do {
+        if (s.Current_ == s.ColumnsLength_) do {
             if (const auto result = Flow_->FetchValues(ctx, fields); result != EFetchResult::One)
                 return result;
 
             s.Current_ = 0;
-            s.BlockLength_ = GetBlockCount(s.Values_.back());
+            s.ColumnsLength_ = GetBlockCount(*fields[Types_.size()]);
             s.Transpose(fields);
-        } while (!s.BlockLength_);
+        } while (!s.ColumnsLength_);
 
-        for (size_t i = 0; i < Types_.size(); ++i) {
-            if (const auto out = output[i]) {
-                *out = s.Get(i);
-            }
-        }
-
+        s.Get(output);
         s.Current_++;
+
         return EFetchResult::One;
     }
-
-private:
-    struct TState : public TComputationValue<TState> {
-        size_t BlockLength_ = 0;
-        size_t Current_ = 0;
-        TUnboxedValueVector Values_;
-
-        TState(TMemoryUsageInfo* memInfo, const TVector<TType*>& types)
-            : TComputationValue(memInfo)
-            , Values_(types.size() + 1U)
-        {}
-
-        void ClearValues() {
-            Values_.assign(Values_.size(), NUdf::TUnboxedValuePod());
-        }
-
-        void Transpose(NUdf::TUnboxedValue*const* fields) {
-            for (int i = 0; i < Values_.size() - 1; ++i) {
-                const ui8* byte_buffer = TArrowBlock::From(fields[i]).GetDatum().array()->template GetValues<uint8_t>(1);
-                size_t item_size = CalcMaxBlockItemSize(fields[i]);
-                // TODO: 1) apply MergeColumns,
-                // 2) Convert output of MergeColumns to table of TUnboxedValues and
-                // 3) save this table of TUnboxedValues in state
-                // Table == vector of wide
-            }
-        }
-
-        NUdf::TUnboxedValuePod Get(size_t idx) const {
-            // TODO
-            // Get single TUnboxedValue
-            // May be we can give Wide row instead of single values
-        }
-    };
-
-    void RegisterDependencies() const final {
-        FlowDependsOn(Flow_);
-    }
-
-    void MakeState(NUdf::TUnboxedValue& state) const {
-        state = ctx.HolderFactory.Create<TState>(Types_);
-    }
-
-    TState& GetState(NUdf::TUnboxedValue& state) const {
-        if (!state.HasValue()) {
-            MakeState(state);
-        }
-        return *static_cast<TState*>(state.AsBoxed().Get());
-    }
-
-    IComputationWideFlowNode* const Flow_;
-    const TVector<TType*> Types_;
-    const size_t WideFieldsIndex_;
-
-public:
 #ifndef MKQL_DISABLE_CODEGEN
     ICodegeneratorInlineWideNode::TGenerateResult DoGenGetValues(const TCodegenContext& ctx, Value* statePtr, BasicBlock*& block) const {
         auto& context = ctx.Codegen.GetContext();
@@ -344,6 +303,122 @@ public:
 #endif
 
 private:
+    struct TState : public TComputationValue<TState> {
+        size_t ColumnsLength_ = 0;
+        size_t Current_ = 0;
+        size_t ColumnsCount_ = 0;
+        const TVector<TType*>& Types_;
+        NKikimr::TAlignedPagePool& Pool_;
+        i8* Buffer_ = nullptr;
+        size_t Sizes_[4]; // TODO: Change 4 for some N
+
+        TState(TMemoryUsageInfo* memInfo, const TVector<TType*>& types, TComputationContext& ctx)
+            : TComputationValue(memInfo)
+            , ColumnsCount_(types.size())
+            , Types_(types)
+            , Pool_(ctx.HolderFactory.GetPagePool())
+            , Buffer_(reinterpret_cast<i8*>(Pool_.GetBlock(NKikimr::TAlignedPagePool::POOL_PAGE_SIZE)))
+        {
+            Y_ASSERT(ColumnsCount_ == 4 && "More than 4 columns not supported yet");
+        }
+
+        ~TState() {
+            Pool_.ReturnBlock(Buffer_, NKikimr::TAlignedPagePool::POOL_PAGE_SIZE);
+        }
+
+        void Transpose(NUdf::TUnboxedValue*const* fields) {
+            i8* data[4]; // TODO: Change 4 for some N
+
+            for (size_t i = 0; i < ColumnsCount_; ++i) {
+                auto array = TArrowBlock::From(*fields[i]).GetDatum().array();
+
+                Sizes_[i] = CalcMaxBlockItemSize(Types_[i]);
+                data[i] = array->template GetValues<i8>(1);
+            }
+
+            // TODO: Change 4 for some N
+            Y_ASSERT(std::accumulate(Sizes_, Sizes_ + 4, 0) * ColumnsLength_ <= NKikimr::TAlignedPagePool::POOL_PAGE_SIZE && "Total size of block should be less than 64KB");
+
+            NSimd::Perfomancer performancer;
+            auto worker = NSimd::ChooseTrait(performancer);
+            worker->MergeColumns(Buffer_, data, Sizes_, ColumnsLength_);
+        }
+
+        void Get(NUdf::TUnboxedValue*const* output) const {
+            auto total_row_size = std::accumulate(Sizes_, Sizes_ + 4, 0); // TODO: Change 4 for some N
+            auto offset_buffer = Buffer_ + Current_ * total_row_size;
+
+            for (size_t i = 0; i < ColumnsCount_; ++i) {
+                if (const auto out = output[i]) {
+                    auto dataType = static_cast<TDataType*>(Types_[i]);
+                    switch (dataType->GetSchemeType()) {
+                        case NUdf::TDataType<bool>::Id:
+                        {
+                            *out = NUdf::TUnboxedValuePod(*reinterpret_cast<bool*>(offset_buffer));
+                            break;
+                        }
+                        case NUdf::TDataType<i8>::Id:
+                        {
+                            *out = NUdf::TUnboxedValuePod(*reinterpret_cast<i8*>(offset_buffer));
+                            break;
+                        }
+                        case NUdf::TDataType<ui8>::Id:
+                        {
+                            *out = NUdf::TUnboxedValuePod(*reinterpret_cast<ui8*>(offset_buffer));
+                            break;
+                        }
+                        case NUdf::TDataType<i16>::Id:
+                        {
+                            *out = NUdf::TUnboxedValuePod(*reinterpret_cast<i16*>(offset_buffer));
+                            break;
+                        }
+                        case NUdf::TDataType<ui16>::Id:
+                        {
+                            *out = NUdf::TUnboxedValuePod(*reinterpret_cast<ui16*>(offset_buffer));
+                            break;
+                        }
+                        case NUdf::TDataType<i32>::Id:
+                        {
+                            *out = NUdf::TUnboxedValuePod(*reinterpret_cast<i32*>(offset_buffer));
+                            break;
+                        }
+                        case NUdf::TDataType<ui32>::Id:
+                        {
+                            *out = NUdf::TUnboxedValuePod(*reinterpret_cast<ui32*>(offset_buffer));
+                            break;
+                        }
+                        case NUdf::TDataType<float>::Id:
+                        {
+                            *out = NUdf::TUnboxedValuePod(*reinterpret_cast<float*>(offset_buffer));
+                            break;
+                        }
+                        case NUdf::TDataType<i64>::Id:
+                        {
+                            *out = NUdf::TUnboxedValuePod(*reinterpret_cast<i64*>(offset_buffer));
+                            break;
+                        }
+                        case NUdf::TDataType<ui64>::Id:
+                        {
+                            *out = NUdf::TUnboxedValuePod(*reinterpret_cast<ui64*>(offset_buffer));
+                            break;
+                        }
+                        case NUdf::TDataType<double>::Id:
+                        {
+                            *out = NUdf::TUnboxedValuePod(*reinterpret_cast<double*>(offset_buffer));
+                            break;
+                        }
+                        default:
+                        {
+                            // TODO: Support all types conversion
+                            Y_ASSERT(false && "Type not supported yet");
+                            break;
+                        }
+                    }
+                    offset_buffer += Sizes_[i];
+                }
+            }
+        }
+    };
 #ifndef MKQL_DISABLE_CODEGEN
     class TLLVMFieldsStructureState: public TLLVMFieldsStructure<TComputationValue<TState>> {
     private:
@@ -389,6 +464,25 @@ private:
         {}
     };
 #endif
+
+    void RegisterDependencies() const final {
+        FlowDependsOn(Flow_);
+    }
+
+    void MakeState(TComputationContext& ctx, NUdf::TUnboxedValue& state) const {
+        state = ctx.HolderFactory.Create<TState>(Types_, ctx);
+    }
+
+    TState& GetState(NUdf::TUnboxedValue& state, TComputationContext& ctx) const {
+        if (!state.HasValue()) {
+            MakeState(ctx, state);
+        }
+        return *static_cast<TState*>(state.AsBoxed().Get());
+    }
+
+    IComputationWideFlowNode* const Flow_;
+    const TVector<TType*> Types_;
+    const size_t WideFieldsIndex_;
 };
 
 IComputationNode* WrapTestFromBlocks2(TCallable& callable, const TComputationNodeFactoryContext& ctx) {
@@ -442,19 +536,14 @@ TRuntimeNode MakeFlow(TSetup<LLVM>& setup) {
     return TRuntimeNode(callableBuilder.Build(), false);
 }
 
-// TODO: unimplemented
-// template<bool LLVM>
-// TRuntimeNode MakeFromBlocks2(TSetup<LLVM>& setup) {
-//     TProgramBuilder& pb = *setup.PgmBuilder;
-//     TCallableBuilder callableBuilder(*setup.Env, "TestFromBlocks2",
-//                                      pb.NewFlowType(
-//                                          pb.NewMultiType({
-//                                              pb.NewBlockType(pb.NewDataType(NUdf::EDataSlot::Uint64), TBlockType::EShape::Many),
-//                                              pb.NewBlockType(pb.NewDataType(NUdf::EDataSlot::Uint64), TBlockType::EShape::Scalar),
-//                                              pb.NewBlockType(pb.NewDataType(NUdf::EDataSlot::Uint64), TBlockType::EShape::Scalar),
-//                                              })));
-//     return TRuntimeNode(callableBuilder.Build(), false);
-// }
+TRuntimeNode TProgramBuilder::WideFromBlocks2(TRuntimeNode flow) {
+    auto outputItems = ValidateBlockFlowType(flow.GetStaticType());
+    outputItems.pop_back();
+    TType* outputMultiType = NewMultiType(outputItems);
+    TCallableBuilder callableBuilder(Env, "TestFromBlocks2", NewFlowType(outputMultiType));
+    callableBuilder.Add(flow);
+    return TRuntimeNode(callableBuilder.Build(), false);
+}
 
 } // namespace
 
